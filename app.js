0ebd0ad0096213835f5ed5f306fe0e5a81781ce1
leogirardeau <leogir.pro@gmail.com>
2020-08-10 11:05:57 +0200

    add ;

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ const imgText = document.querySelector(".caption");
 previews.forEach(preview => {
     preview.addEventListener("click", () => {
         modal.classList.add("open");
-    })
-})
+    });
+});
