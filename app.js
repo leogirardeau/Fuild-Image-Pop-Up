93bb6c5667619e94e16f9ee2586192e857c0d8fe
leogirardeau <leogir.pro@gmail.com>
2020-08-10 11:04:59 +0200

    add click event function

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ const modal = document.querySelector(".modal");
 const previews = document.querySelectorAll(".gallery img");
 const original = document.querySelector(".full-img");
 const imgText = document.querySelector(".caption");
+
+previews.forEach(preview => {
+    preview.addEventListener("click", () => {
+        modal.classList.add("open");
+    })
+})
